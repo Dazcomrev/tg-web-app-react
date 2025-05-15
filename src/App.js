0ffbbe136b329ee611ed9c9df88a258d46a95eac
Dazcomrev <151546169+Dazcomrev@users.noreply.github.com>
2025-05-15 19:07:23 +0700

    Внесены изменения

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from "./components/Header/Header";
 //import Button from './components/Button/Button';
 import { Route, Routes } from 'react-router-dom';
 import PlayerCard from './components/PlayerCard/PlayerCard';
+import ListTeams from './components/ListTeams/ListTeams';
 
 function App() {
     const { onToggleButton, tg } = useTelegram();
