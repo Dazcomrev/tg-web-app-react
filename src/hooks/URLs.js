c99ef9b0cf62be001d44afe900146ee182d7a73e
Dazcomrev <151546169+Dazcomrev@users.noreply.github.com>
2025-06-01 18:11:20 +0700

    Внесение изменений 01.06.2025 18:11.

--- src/hooks/URLs.js
+++ src/hooks/URLs.js
@@ -1,5 +1,5 @@
 ﻿const urlWebApp = 'https://aesthetic-creponne-ffd0c8.netlify.app/';
-const urlServer = 'http://server-production-ff27.up.railway.app/'//'http://localhost:5000/';
+const urlServer = 'https://server-production-ff27.up.railway.app/'//'http://localhost:5000/';
 export function useURL() {
     return {
         urlWebApp,
